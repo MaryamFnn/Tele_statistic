ffb45d7a83bbb0e1d504748ef122cbeb06182f85
MaryamFnn <farahani.n72@gmail.com>
2022-12-03 14:15:36 +0100

    updated commit

--- src/chat-statistics/stats.py
+++ src/chat-statistics/stats.py
@@ -12,8 +12,14 @@ from wordcloud import WordCloud
 
 
 class ChatStatistics:
-
-    def __init__(self, chat_json):
+    """Generate chat statistics from telegram chat jason file 
+    """
+    def __init__(self, chat_json : Union[str, Path]):
+        """
+        Args:
+            chat_json (Union[str, Path]): path to telegram jason file
+        """
+        logger.info(f'loading chat data from {chat_json}')
         with open (chat_json) as f:
             self.chat_data = json.load(f)
         self.normalizer = Normalizer()
@@ -21,28 +27,29 @@ class ChatStatistics:
         stop_words = list(map(str.strip, stop_words))
         self.stop_words = list(map(self.normalizer.normalize, stop_words))
 
-
-
     def generate_word_cloud(self,output_dir):
+        """_summary_
+        Args:
+            output_dir (_type_): path to output directory for word cloud image
+        """
         text_content = ''
-
         for msg in self.chat_data['messages']:
             if type (msg['text']) is str:
                 tokens = word_tokenize(msg['text'])
                 tokens=list(filter(lambda item:item not in self.stop_words,tokens))
                 text_content += f" {' '.join(tokens)}"
+        logger.info('Generating word cloud...')
+        # normilize, reshape for final wordcloud
         text_content = self.normalizer.normalize(text_content)
         text_content=text_content[:20000]
         text_content = arabic_reshaper.reshape(text_content)
         text_content = get_display(text_content)
+        # generate word cloud
         wordcloud = WordCloud(font_path = str(DATA_DIR/'BHoma.ttf'),background_color='white').generate(text_content)
         wordcloud.to_file(str(Path(output_dir)/'wordcloud.png'))
-
-
-
-
 
 
 if __name__=="__main__":
     chat_stats = ChatStatistics(chat_json=DATA_DIR/'result.json')
     chat_stats.generate_word_cloud(output_dir=DATA_DIR)
+    print("Done!")
